6b27d508eebef96a5f103f2a213cd30dc3fb1a65
maxstar <maksimgavura78@gmail.com>
2024-12-09 00:27:24 +0200

    Добавьте файлы проекта.

---
+++ Lesson#6Task2/Lesson_6Task2.py
@@ -0,0 +1,9 @@
+﻿x = int(input('vvedite naturalnoe 4islo: '))
+if x <= 0:
+    print('vvedeno ne naturalnoe 4islo: ')
+else:
+    division = 0
+    for i in range(1, x):
+        if x % i == 0:
+            division += 1
+print(division)
